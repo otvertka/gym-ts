349a3fa491d9f65d78dbe8762bb52d58e66eb264
otvertka <otvertka123@ukr.net>
2025-03-01 13:38:22 +0100

    realized contact page

---
+++ src/scenes/contactUs/index.tsx
@@ -0,0 +1,142 @@
+import { useForm } from "react-hook-form";
+import { SelectedPage } from "@/shared/types";
+import { motion } from 'framer-motion';
+import ContactUsPageGraphic from "@/assets/ContactUsPageGraphic.png";
+import HText from "@/shared/HText";
+import { p } from "framer-motion/client";
+
+type Props = {
+    setSelectedPage: (value: SelectedPage) => void;
+}
+
+const ContactUs = ({ setSelectedPage }: Props) => {
+    const inputStyles = `mb-5 w-full rounded-lg bg-primary-500 px-5 py-3 placeholder-white`;
+    const { register, trigger, formState: { errors } } = useForm();
+    const onSubmit = async (e: any) => {
+        const isValid = await trigger();
+        if (!isValid) {
+            e.preventDefault();
+        }
+    }
+    return (
+        <section id="contactus" className="mx-auto w-5/6 pt-24 pb-32">
+            <motion.div onViewportEnter={() => setSelectedPage(SelectedPage.ContactUs)}>
+                {/* HEADER */}
+
+                <motion.div
+                    className="md:-mt-20"
+                    initial="hidden"
+                    whileInView="visible"
+                    viewport={{ once: true, amount: 0.5 }}
+                    transition={{ duration: 0.5 }}
+                    variants={{
+                        hidden: { opacity: 0, x: -50 },
+                        visible: { opacity: 1, x: 0 }
+                    }}>
+                    <HText>
+                        <span className="text-primary-500">Join now</span> to get in shape
+                    </HText>
+                    <p className="my-5">
+                        Pulvinar fames vitae vitae quis. Quis amet vulputate tincidunt at in nulla nec.
+                        Consequat sed facilisis dui sit egestas ultrices tellus.
+                        Ullamcorper arcu id pretium sapien proin integer nisl. Felis orci diam odio.
+                    </p>
+                </motion.div>
+                {/* FORM & IMAGE */}
+                <div className="mt-10 justify-between gap-8 md:flex">
+                    <motion.div
+                        className="mt-10 basis-3/5 md:mt-0"
+                        initial="hidden"
+                        whileInView="visible"
+                        viewport={{ once: true, amount: 0.5 }}
+                        transition={{ duration: 0.5 }}
+                        variants={{
+                            hidden: { opacity: 0, y: 50 },
+                            visible: { opacity: 1, y: 0 }
+                        }}>
+
+                        <form
+                            target="_blank"
+                            onSubmit={onSubmit}
+                            action="https://formsubmit.co/1d43d836a485b5154846a8d4382c731b"
+                            method="POST">
+                            <input
+                                className={inputStyles}
+                                type="text"
+                                placeholder="NAME"
+                                {...register("name", {
+                                    required: true,
+                                    maxLength: 100,
+                                })} />
+                            {errors.name && (
+                                <p className="mt-1 text-primary-500">
+                                    {errors.name.type === "required" && "This field is required."}
+                                    {errors.name.type === "maxLength" && "Max length is 100"}
+                                </p>
+                            )}
+                            <input
+                                className={inputStyles}
+                                type="email"
+                                placeholder="EMAIL"
+                                {...register("email", {
+                                    required: true,
+                                    pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                                })} />
+                            {errors.name && (
+                                <p className="mt-1 text-primary-500">
+                                    {errors.name.type === "required" && "This field is required."}
+                                    {errors.name.type === "pattern" && "Invalid e-mail address."}
+                                </p>
+                            )}
+                            <textarea
+                                className={inputStyles}
+                                placeholder="MESSAGE"
+                                rows={4}
+                                cols={50}
+                                {...register("message", {
+                                    required: true,
+                                    maxLength: 2000,
+                                })} />
+                            {errors.message && (
+                                <p className="mt-1 text-primary-500">
+                                    {errors.message.type === "required" && "This field is required."}
+                                    {errors.message.type === "maxLength" && "Max length is 2000 char."}
+                                </p>
+                            )}
+
+                            <button
+                                type="submit"
+                                className="mt-5 rounded-lg bg-secondary-500 px-20 py-3 hover:scale-110 transition-all">
+                                SUBMIT
+                            </button>
+                        </form>
+                    </motion.div>
+
+                    <motion.div
+                        className="relative mt-16 basis-2/5 md:mt-0"
+                        initial="hidden"
+                        whileInView="visible"
+                        viewport={{ once: true, amount: 0.5 }}
+                        transition={{ delay: 0.2, duration: 0.5 }}
+                        variants={{
+                            hidden: { opacity: 0, y: 50 },
+                            visible: { opacity: 1, y: 0 }
+                        }}>
+
+                        <div
+                            className="w-full before:absolute before:-bottom-20 before:-right-10 before:z-[-1] md:before:content-evolvetext">
+                            <img
+                                className="w-full"
+                                src={ContactUsPageGraphic}
+                                alt="contact-us-page-graphic" />
+                        </div>
+
+                    </motion.div>
+                </div>
+            </motion.div>
+
+        </section>
+    )
+}
+
+export default ContactUs
